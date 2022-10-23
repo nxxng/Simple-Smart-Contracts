bf24f83fc1d2e70d0db37a8c093abcd3f1d1fd09
Necooo7 <necati.nct.42@gmail.com>
2022-10-23 22:46:01 +0300

    Added new contracts

---
+++ contracts/events.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.10;
+
+contract events {
+    // Declare an event which logs an address and a string
+    event TestCalled(address sender, string message);
+
+    function test() public {
+        // Log an event
+        emit TestCalled(msg.sender, "Someone called test()!");
+    }
+
+}
